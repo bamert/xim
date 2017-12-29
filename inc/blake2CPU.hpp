7a800ab1f4c32da27aef343ecf5d5408f2ab3b10
Nik Bamert <bamertn@sans-lab-22.ethz.ch>
2017-12-29 10:50:57 +0100

    starting to factor stuff out of the loop

--- inc/blake2CPU.hpp
+++ inc/blake2CPU.hpp
@@ -104,67 +104,7 @@ namespace ndb {
     v[c] = v[c] + v[d];             \
     v[b] = ROTR64(v[b] ^ v[c], 63); }
 
-struct work {
-  unsigned char data[256];
-  unsigned char midstate[32];
-  unsigned char target[32];
-  unsigned char hash[32];
 
-  unsigned char device_target[32];
-  double    device_diff;
-  double    share_diff;
-
-  int   rolls;
-  int   drv_rolllimit; /* How much the driver can roll ntime */
-
-// dev_blk_ctx blk;
-
-  struct thr_info *thr;
-  int   thr_id;
-  struct pool *pool;
-  struct timeval  tv_staged;
-
-  bool    mined;
-  bool    clone;
-  bool    cloned;
-  int   rolltime;
-  bool    longpoll;
-  bool    stale;
-  bool    mandatory;
-  bool    block;
-
-  bool    stratum;
-  char    *job_id;
-  uint64_t  nonce2;
-  size_t    nonce2_len;
-  char    *ntime;
-  double    sdiff;
-  char    *nonce1;
-
-  bool    gbt;
-  char    *coinbase;
-  int   gbt_txns;
-
-  unsigned int  work_block;
-  int   id;
-// UT_hash_handle  hh;
-
-  double    work_difficulty;
-
-  // Allow devices to identify work if multiple sub-devices
-  int   subid;
-  // Allow devices to flag work for their own purposes
-  bool    devflag;
-  // Allow devices to timestamp work for their own purposes
-  struct timeval  tv_stamp;
-
-  struct timeval  tv_getwork;
-  struct timeval  tv_getwork_reply;
-  struct timeval  tv_cloned;
-  struct timeval  tv_work_start;
-  struct timeval  tv_work_found;
-  char    getwork_mode;
-};
 uint32_t swab32(uint32_t v) {
   return bswap_32(v);
 }
@@ -226,6 +166,25 @@ class Blake2bCPU {
     blake2b_ctx ctx;
     uint8_t hash[32];
     bool found = false;
+
+    const uint8_t sigma[12][16] = {
+      { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
+      { 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
+      { 11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4 },
+      { 7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8 },
+      { 9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13 },
+      { 2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9 },
+      { 12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11 },
+      { 13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10 },
+      { 6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5 },
+      { 10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0 },
+      { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
+      { 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 }
+    };
+    int i;
+    uint64_t v[16], m[16];
+
+
     /*
         for (i = 0; i < 8; i++)             // state, "param block"
           ctx->h[i] = blake2b_iv[i];
@@ -259,39 +218,71 @@ class Blake2bCPU {
         ctx.h[i] = blake2b_iv[i];
       ctx.h[0] ^= 0x01010000 ^ (0 << 8) ^ 32;
 
+         //This is the same on every iteration
+      for (int i = 0; i < 8; i++) {           // init work variables
+        v[i] = ctx.h[i];
+        v[i + 8] = blake2b_iv[i];
+      }
+
+      v[12] ^= ctx.t[0];                 // low 64 bits of offset
+      v[13] ^= ctx.t[1];                 // high 64 bits
+      v[14] = ~v[14];   //it's always the last block.
+
+
       ctx.t[0] = 0;                      // input count low word
       ctx.t[1] = 0;                      // input count high word
       ctx.c = 0;                         // pointer within buffer
       ctx.outlen = 32;
 
-      for (int i = 0; i < 128; i++)      // zero input block
+      //pre-pad input block
+      for (int i = 0; i < 128; i++)
         ctx.b[i] = 0;
 
-
-      //update:
+      //---start update
+      //fill header:
       for (int i = 0; i < 80; i++) {
-        ctx.b[ctx.c++] = header[i];
+        ctx.b[i] = header[i];
+      }
+      //set input data length
+      ctx.c = 80;
+      //---end update
+
+      //---start final
+      ctx.t[0] = 80;                // mark last block offset
+
+
+      //blake2b_compress(&ctx, 1);           // final block flag = 1
+      //------------start compress:
+
+
+   
+      for (int i = 0; i < 16; i++)            // get little-endian words
+        m[i] = B2B_GET64(&ctx.b[8 * i]);
+
+      for (int i = 0; i < 12; i++) {          // twelve rounds
+        B2B_G( 0, 4,  8, 12, m[sigma[i][ 0]], m[sigma[i][ 1]]);
+        B2B_G( 1, 5,  9, 13, m[sigma[i][ 2]], m[sigma[i][ 3]]);
+        B2B_G( 2, 6, 10, 14, m[sigma[i][ 4]], m[sigma[i][ 5]]);
+        B2B_G( 3, 7, 11, 15, m[sigma[i][ 6]], m[sigma[i][ 7]]);
+        B2B_G( 0, 5, 10, 15, m[sigma[i][ 8]], m[sigma[i][ 9]]);
+        B2B_G( 1, 6, 11, 12, m[sigma[i][10]], m[sigma[i][11]]);
+        B2B_G( 2, 7,  8, 13, m[sigma[i][12]], m[sigma[i][13]]);
+        B2B_G( 3, 4,  9, 14, m[sigma[i][14]], m[sigma[i][15]]);
       }
 
+      for (int  i = 0; i < 8; ++i )
+        ctx.h[i] ^= v[i] ^ v[i + 8];
 
-      //final
+      //----------------End compress
 
-      ctx.t[0] += ctx.c;                // mark last block offset
-      if (ctx.t[0] < ctx.c)             // carry overflow
-        ctx.t[1]++;                    // high word
-
-      //No need. it's already filled with zeros.
-      //while (ctx.c < 128)                // fill up with zeros
-      //  ctx.b[ctx.c++] = 0;
-      
-
-      blake2b_compress(&ctx, 1);           // final block flag = 1
 
       // little endian convert and store
       for (int i = 0; i < ctx.outlen; i++) {
         hash[i] =
           (ctx.h[i >> 3] >> (8 * (i & 7))) & 0xFF;
       }
+      //-------End final
+
 
       //blake2b_update(&ctx, header, 80);
       //blake2b_final(&ctx, &hash);
@@ -314,25 +305,6 @@ class Blake2bCPU {
     return false;
   }
 
-  void sia_gen_hash(const unsigned char *data, unsigned int len, unsigned char *hash) {
-    blake2b_ctx ctx;
-    blake2b_init(&ctx, 32, NULL, 0);
-    blake2b_update(&ctx, data, len);
-    blake2b_final(&ctx, hash);
-  }
-
-  void sia_regenhash(struct work *work) {
-    uint32_t data[20];
-    uint32_t hash[16];
-    char *scratchbuf;
-    uint32_t *nonce = (uint32_t *)(work->data + 32);
-    uint32_t *ohash = (uint32_t *)(work->hash);
-
-    be32enc_vect(data, (const uint32_t *)work->data, 20);
-    data[8] = htobe32(*nonce);
-    sia_gen_hash((const unsigned char *)data, 80, (unsigned char *)hash);
-    swab256(ohash, hash);
-  }
 
 // Initialization Vector.
   const uint64_t blake2b_iv[8] = {
